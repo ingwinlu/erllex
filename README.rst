4985f75eba357788ecd79c358c397595cee4975b
winlu <derwinlu+git@gmail.com>
2015-04-08 17:04:46 +0200

    init

---
+++ README.rst
@@ -0,0 +1,4 @@
+erllex
+======
+
+A simple regex lexer sample programm to showoff erlang regular expressions and subgroup matching.
---
+++ erllex.erl
@@ -0,0 +1,46 @@
+-module(erllex).
+-export([get_rules/0, tokenize/2]).
+
+get_rules() -> 
+    [
+        "(?P<NUMBER>\\d+)",
+        "(?P<IDENTIFIER>[a-zA-Z_]\\w+)",
+        "(?P<PLUS>\\+)",
+        "(?P<MINUS>\\-)",
+        "(?P<WHITESPACE>\\s)"
+    ].
+
+tokenize(BinaryString, Rules) ->
+    ConcatRules = concat_rules(Rules),
+    {ok, CompiledRules} = re:compile(ConcatRules, [anchored]),
+    {namelist, Namelist} = re:inspect(CompiledRules, namelist),
+    ReversedTokens = get_tokens(BinaryString, CompiledRules, Namelist, []),
+    lists:reverse(ReversedTokens).
+
+get_tokens(<<>>, _Rules, _Namelist, Acc) ->
+    Acc;
+get_tokens(BinaryString, Rules, Namelist, Acc) ->
+    {match, Matchlist} = re:run(BinaryString,Rules,[{capture,all_names,binary}]),
+    Token = extract_token(Namelist, Matchlist),
+    {_, Matched} = Token,
+    MatchedLength = byte_size(Matched),
+    <<_Matched:MatchedLength/binary, NewBinaryString/binary>> = BinaryString,
+    NewAcc = [Token | Acc],
+    get_tokens(NewBinaryString, Rules, Namelist, NewAcc).
+
+%%%PRIV
+concat_rules(NaiveRuleset) ->
+    lists:foldl(
+        fun(Rule, Acc) ->
+            Acc ++ Rule ++ "|"
+        end,
+        [],
+        NaiveRuleset
+    ).
+
+extract_token([_Name | Namelist], [<<>> | Matchlist]) ->
+    extract_token(Namelist, Matchlist);
+extract_token([Name | _Namelist], [Match | _Matchlist]) ->
+    {Name, Match};
+extract_token(_, _) ->
+    {error, nomatch}.
